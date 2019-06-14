67a546e33fe68e6129ce326b7a51f35893f92730
kyawswaraung <kyawswaaung87@gmail.com>
2019-06-14 11:22:54 +0630

    init

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 class HelloWorld {
 	
 	public static void main(String[] args) {
+		System.out.println("start....");
 		for(int i=0; i< 10; i++) {
 			System.out.println("index " + i);
 		}
+		System.out.println("End....");
 	}
 }
